4c1eb356379804558ab198b18278addf61112996
Matheus <mthsferreirap@gmail.com>
2023-12-20 16:04:02 -0300

    chore: Add fetch update

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Button } from 'react-native';
+import * as Updates from 'expo-updates';
 
-export default function App() {
+function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Teste 2!!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button title="Fetch update" onPress={onFetchUpdateAsync} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
