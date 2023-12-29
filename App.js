470eb2c306dc19198045e31f57e2287a20bf9c6f
Matheus <mthsferreirap@gmail.com>
2023-12-29 11:49:04 -0300

    Chore: Change App

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { View, Button } from 'react-native';
-import * as Updates from 'expo-updates';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function App() {
-  async function onFetchUpdateAsync() {
-    
-  }
-
-  return (
-    <View>
-      <Button title="aaaaaaaa" onPress={onFetchUpdateAsync} />
-    </View>
-  );
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
